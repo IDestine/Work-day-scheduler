921e3c5dcc4eeec8c9d3e453abab570be2244d47
Isaiah Destine <isaiahdestine@gmail.com>
2022-10-30 22:46:06 -0400

    js done

--- script.js
+++ script.js
@@ -7,15 +7,14 @@ var now = moment().format("H A");
 
 /* planWorkday */
 var planWorkday = [
-          {time: "9 AM", event:""}
-          {time: "10 AM", event:""}
-          {time: "11 AM", event:""}
-          {time: "12 PM", event:""}
-          {time: "1 PM", event:""}
-          {time: "2 PM", event:""}
-          {time: "3 PM", event:""}
-          {time: "4 PM", event:""}
-          {time: "5 PM", event:""}
+          {time: "9 AM", event:""},
+          {time: "11 AM", event:""},
+          {time: "12 PM", event:""},
+          {time: "1 PM", event:""},
+          {time: "2 PM", event:""},
+          {time: "3 PM", event:""},
+          {time: "4 PM", event:""},
+          {time: "5 PM", event:""},
 ];
 
 /* Local Storage */
@@ -58,3 +57,22 @@ function colorRow(time) {
 	}
 } 
 
+
+/* Save */
+$(".saveBtn").on("click", function() {
+	var blockID = parseInt(
+		$(this)
+			.closest(".time-block")
+			.attr("id")
+	);
+	var userEntry = $.trim(
+		$(this)
+			.parent()
+			.siblings("textarea")
+			.val()
+	);
+	planWorkday[blockID].event = userEntry;
+
+	/* Set local storage */
+	localStorage.setItem("workDay", JSON.stringify(planWorkday));
+});
